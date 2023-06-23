7574b224d56cb91e17be9e854aece1f1732585d6
B0lemix <109000296+B0lemix@users.noreply.github.com>
2023-06-23 09:25:39 +0200

    edited logo: Improve height and resolution

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import {
 } from 'firebase/firestore';
 
 const style = {
-   bg: 'h-screen w-screen p-4 bg-gradient-to-r from-[#b1cab2] to-[#baed8e] ',
+   bg: ' w-full p-4 bg-gradient-to-r from-[#b1cab2] to-[#baed8e] ',
    img: 'object-cover h-36 w-full rounded-lg  ',
    container: 'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-2xl p-4 font-semibold',
    heading: 'text-3xl font-bold text-center text-gray-800 p-2',
@@ -119,12 +119,12 @@ function App() {
                ))}
             </ul>
 
-            <a className="group text-pink-500 transition-all duration-300 ease-in-out" href="#">
+            {/*             <a className="group text-pink-500 transition-all duration-300 ease-in-out" href="#">
                <span className="bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
                   This text gets on hover
                </span>
             </a>
-
+ */}
             {data.length < 1 ? null : (
                <p className={style.count}>{`Te quedan ${
                   data.filter((pendientes) => pendientes.completed === false).length
